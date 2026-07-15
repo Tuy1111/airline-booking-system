44ac5a81234243aeb8677ff8c81cb9967a6eae2e
yingmod <vinhos51104@gmail.com>
2026-07-15 11:36:37 +0700

    feat: Integrate Keycloak OAuth2 Login in API Gateway

--- infra/docker/init-databases.sql
+++ infra/docker/init-databases.sql
@@ -6,6 +6,7 @@ CREATE DATABASE user_db;
 CREATE DATABASE booking_db;
 CREATE DATABASE payment_db;
 CREATE DATABASE notify_db;
+CREATE DATABASE keycloak;
 
 -- Grant đầy đủ quyền cho user abs (đã tạo bởi POSTGRES_USER)
 GRANT ALL PRIVILEGES ON DATABASE flight_db   TO abs;
@@ -13,3 +14,4 @@ GRANT ALL PRIVILEGES ON DATABASE user_db     TO abs;
 GRANT ALL PRIVILEGES ON DATABASE booking_db  TO abs;
 GRANT ALL PRIVILEGES ON DATABASE payment_db  TO abs;
 GRANT ALL PRIVILEGES ON DATABASE notify_db   TO abs;
+GRANT ALL PRIVILEGES ON DATABASE keycloak    TO abs;

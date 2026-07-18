e5f629fdebbbc19ead8ff7e4bc498feaab62d4f8
TuyNG <ngogiantuy11112005@gmail.com>
2026-07-18 17:26:45 +0700

    feat: add initialization scripts for multiple databases including Keycloak

--- infra/docker/postgres/init-multi-db.sh
+++ infra/docker/postgres/init-multi-db.sh
@@ -2,7 +2,7 @@
 # Creates one DB per service on container init.
 set -e
 
-for db in flight_db booking_db user_db payment_db notify_db; do
+for db in flight_db booking_db user_db payment_db notify_db keycloak; do
   echo "Creating database: $db"
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
     CREATE DATABASE $db;
